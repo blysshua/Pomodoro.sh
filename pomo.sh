ed0a4feedd2257324bd92ecf173200b8bc6a5d53
blysshua <Dreamworks_Studios@protonmail.com>
2025-10-05 17:13:18 +1100

    Create pomo.sh

---
+++ pomo.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+clear
+#cat ~/Projects/test/pom.txt
+
+set_intervals() {
+  echo "Set study time: (minutes) "
+  read study_time
+  echo "Set break time: (minutes)"
+  read break_time
+}
+
+set_repetitions() {
+  echo "Set repetitions:"
+  read repetitions
+}
+
+set_intervals
+set_repetitions
+
+for ((i = 1; i <= repetitions; i++)); do
+  clear
+  echo "Lock-in: Rep $i/$repetitions"
+  timer "${study_time}m"
+  notify-send "Pomodoro" "Break Time"
+  #notify-send "Pomodoro" "Break Time"
+  #notify-send "Pomodoro" "Break Time"
+  #paplay ~/Music/soft.wav
+  clear
+
+  echo "Break: Rep $i/$repetitions"
+  timer "${break_time}m"
+  notify-send "Pomodoro" "Back to study"
+  #notify-send "Pomodoro" "Back to study"
+  #notify-send "Pomodoro" "Back to study"
+  #paplay ~/Music/soft.wav
+  clear
+done
+
+echo "Session completed!"
